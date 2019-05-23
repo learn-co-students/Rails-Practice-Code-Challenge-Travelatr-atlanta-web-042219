e831daf224cf4eb1a6ad4a6a6e6b287357182397
Will Jones <willjones2010@gmail.com>
2019-05-22 22:17:21 -0400

    Done.

--- app/models/blogger.rb
+++ app/models/blogger.rb
@@ -1,3 +1,32 @@
 class Blogger < ApplicationRecord
+  has_many :posts
+  has_many :destinations, through: :posts
+  validates :name, uniqueness: true
+  validates :age, numericality: { greater_than: 0 }
+  validates :bio, length: { minimum: 30 }
+
+  def total_likes
+    array_of_likes = []
+    self.posts.each do |post|
+      array << post.likes
+    end
+    array_of_likes.sum
+  end
+
+  def featured_post
+    most_liked = ''
+    count = 0
+      self.posts.each do |post|
+        if post.likes > count
+          count = post.likes
+          most_liked = post
+        end
+      end
+    most_liked
+  end
+
+
+
+
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :bloggers, except:[:destroy, :edit, :update]
+  resources :destinations, only:[:show]
+  resources :posts, only:[:new, :create, :show, :edit, :update]
+
+
 end
